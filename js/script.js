538d550c69f3c09ab484f4e1c702b1774b75eabc
hadi ahmed <2354hadi.net@gmail.com>
2022-01-11 15:27:42 +0300

    add responsive  to design

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menu_btn=document.querySelector(".menu-btn");
+let nav_bar=document.querySelector(".navBar");
+menu_btn.onclick=function () {
+    // menu_btn.classList.toggle("");
+    nav_bar.classList.toggle("active");
+    
+}
